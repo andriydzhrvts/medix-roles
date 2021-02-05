482f1fa60c60d5405f0ca728d35a9582dc590737
andriydzhrvts <krash2233@gmail.com>
2021-02-05 15:04:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.USER_ROLES = {
+  USER: "user",
+  ADMIN: "admin",
+  SUPERADMIN: "superAdmin",
+};
+exports.USER_ROLES_OPTION = [
+  { value: this.USER_ROLES.USER, label: "User" },
+  { value: this.USER_ROLES.ADMIN, label: "Admin" },
+  { value: this.USER_ROLES.SUPERADMIN, label: "Super Admin" },
+];
